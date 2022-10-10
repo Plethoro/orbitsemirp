32467891e3c6546f1bda0ef280b6f0eadda81b56
Jayden Tucker <jaydenrtucker@gmail.com>
2022-10-09 23:14:33 -0700

    added sections for steam group and discord user counts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSteam, faDiscord } from '@fortawesome/free-brands-svg-icons'
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,22 @@ const Home: NextPage = () => {
       <div id={styles.homePage}>
         <main className={styles.main}>
           <h1>Lorem Ipsum!</h1>
+
+          <section id={styles.groupInformation}>
+            <div className={styles.group}>
+              <div className={styles.iconContainer}>
+                <FontAwesomeIcon icon={faSteam} />
+              </div>
+              <span>876</span>
+            </div>
+
+            <div className={styles.group}>
+              <div className={styles.iconContainer}>
+                <FontAwesomeIcon icon={faDiscord} />
+              </div>
+              <span>1.23K</span>
+            </div>
+          </section>
         </main>
         <aside>
 
