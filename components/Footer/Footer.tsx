ff5754238838cbcacc673b5a739caa1ab521c6f7
Jayden Tucker <jaydenrtucker@gmail.com>
2022-10-11 20:57:08 -0700

    added a link to ninjabattler.ca

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,6 +3,9 @@ import styles from "./Footer.module.scss";
 
 const Footer: ComponentType<{}> = () => (
   <footer id={styles.footer}>
+    <a href='https://ninjabattler.ca' target="_blank" rel="noreferrer">
+      <img src="/ninjabattler.png" />
+    </a>
   </footer>
 )
 
